4c6552181c86ebc581d1bec34d7a61529d5d8f3a
Setareh <sti.ri433@gmail.com>
2025-12-06 09:56:32 +0330

    Add files via upload

---
+++ Program(8).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication145
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Random rnd = new Random();
+
+        Console.WriteLine("teadad array: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = rnd.Next(1, 100);
+
+        Console.WriteLine("maghadir array:");
+        foreach (var x in arr)
+            Console.Write(x + " ");
+        Console.ReadKey();
+        }
+    }
+}
